0224fd673dcd8ea7023c2f5c9e1c108ee2a7c1ca
3L0H55A <yethuaung123.ytu@gmail.com>
2026-07-18 19:38:49 +0630

    Create js/firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "trustify-f324e.firebaseapp.com",
+  projectId: "trustify-f324e",
+  storageBucket: "trustify-f324e.firebasestorage.app",
+  messagingSenderId: "952055889025",
+  appId: "1:952055889025:web:a667882a6249c979f32180"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
