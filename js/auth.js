049cbc2ec2503029e7ebd690e0cfb61bfe839a35
3L0H55A <yethuaung123.ytu@gmail.com>
2026-07-18 19:45:05 +0630

    Create js/auth.js
    Add authentication

---
+++ js/auth.js
@@ -0,0 +1,22 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+import { auth } from "./firebase.js";
+
+// Register
+export async function register(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+// Login
+export async function login(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+// Logout
+export async function logout() {
+  return await signOut(auth);
+}
