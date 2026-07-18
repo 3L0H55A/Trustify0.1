59ba55ab814c9ff9dfb8a42f96ae4c185df47f40
3L0H55A <yethuaung123.ytu@gmail.com>
2026-07-18 19:43:28 +0630

    Update firebase.js
    Add Firebase configuration

--- js/firebase.js
+++ js/firebase.js
@@ -11,7 +11,30 @@ const firebaseConfig = {
   messagingSenderId: "952055889025",
   appId: "1:952055889025:web:a667882a6249c979f32180"
 };
+// Import Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDy3woJe3UcggwiJhfYqQohZrDFadNlj1g",
+  authDomain: "trustify-f324e.firebaseapp.com",
+  projectId: "trustify-f324e",
+  storageBucket: "trustify-f324e.firebasestorage.app",
+  messagingSenderId: "952055889025",
+  appId: "1:952055889025:web:a667882a6249c979f32180",
+  measurementId: "G-0ET919LLJP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
